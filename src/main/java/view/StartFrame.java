cd0819801cf06e5e67d46fc88df1f8e127dc4b0b
xdb14 <xdb14@software.nju.edu.cn>
2016-08-04 11:50:20 +0800

    Development:ui框架

---
+++ src/main/java/view/StartFrame.java
@@ -0,0 +1,26 @@
+package view;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JSeparator;
+
+public class StartFrame implements Observer{
+	private JButton startButton;
+	private JButton exitButton;
+	private JButton helpButton;
+	private JButton settingButton;
+	private JPanel head;
+	private void componentsInstantiation(){
+		
+	}
+	@Override
+	public void update(Observable arg0, Object arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
